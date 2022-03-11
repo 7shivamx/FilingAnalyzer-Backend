e4653771ac0645965e374f696312cd6aaeb4766d
Sanyog Gupta <sanyoggupta42@gmail.com>
2022-03-11 19:16:36 +0530

    Setup and Start scripts added

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 -m venv interiit
+source interiit/bin/activate
+pip install -r requirements.txt
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source interiit/bin/activate
+./interiit/bin/gunicorn --bind 0.0.0.0:5000 wsgi:app
